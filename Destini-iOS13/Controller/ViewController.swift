97dfc9507c258bd161f294b7c549a7b606b564dc
Diego Dmitry <diegodmitry@hotmail.com>
2023-02-05 18:38:14 +0000

    feat: added the last files.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,34 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        updateUI(n: 0)
+        updateUI()
     }
     
 
     @IBAction func choiceMade(_ sender: UIButton) {
         let userAnswer = sender.currentTitle!
-        print(userAnswer)
-        if(userAnswer == "Take a left.") {
-            updateUI(n: 1)
-        } else {
-            updateUI(n: 2)
-        }
-        //updateUI()
+        
+        storyBrain.nextStory(userChoice: userAnswer)
+        
+        updateUI()
     }
     
-    func updateUI(n: Int) {
-        storyLabel.text = stories[n].title
-        choice1Button.setTitle(stories[n].choice1, for: .normal)
-        choice2Button.setTitle(stories[n].choice2, for: .normal)
+    
+    // mudar updateUI para receber parametro
+    func updateUI() {
+        storyLabel.text = storyBrain.storyTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
     }
     
 }
